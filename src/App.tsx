b9b06d6621c79ae15598d960b4babdc13235b4df
Valeria Serci <balerty84@gmail.com>
2024-04-18 12:10:51 +0100

    log the data to the console to test if the fetch is working

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,13 @@ import './App.css'
 function App() {
   const [title, setTitle] = useState('Loading')
 
+  useEffect(() => {
+    fetch('https://craft-purse-backend.vercel.app/')
+      .then(response => response.text())
+      .then(data => console.log(data))
+      .catch(error => { console.log(error) });
+  }, [])
+
   return (
     <>
       {title}
