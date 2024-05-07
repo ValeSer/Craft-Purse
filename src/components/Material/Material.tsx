36613010e30e077a0175651b84b7b91e6d2f51b2
Valeria Serci <balerty84@gmail.com>
2024-05-07 23:38:13 +0100

    disable button while fetching

--- src/components/Material/Material.tsx
+++ src/components/Material/Material.tsx
@@ -3,18 +3,22 @@ import React, { useState }from 'react';
 
 const Material: React.FC = () => {
   const [materials, setMaterials] = useState([])
+  const [loading, setLoading] = useState(false)
 
   const clickHandler = () => {
+    setLoading(true);
     fetch('https://craft-purse-backend.vercel.app/')
       .then(response => response.json())
       .then(data => setMaterials(data))
+      .then(() => setLoading(false))
       .catch(error => { console.log(error) });
+
   }
   return (
     <>
       <div className='container'>
         <div className='text-container'>
-        <button onClick={clickHandler}> Display Materials </button>
+        <button disabled={loading} onClick={clickHandler}> Display Materials </button>
         <div> {materials[0] && materials[0].name}</div>
         </div>
       </div>
