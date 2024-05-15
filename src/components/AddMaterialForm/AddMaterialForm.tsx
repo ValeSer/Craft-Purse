1604dfc2d085dee4008fb12f0c4fa0a2ed80cbc3
Valeria Serci <balerty84@gmail.com>
2024-05-15 21:01:08 +0100

    work in progress, adding a new material. So far returning undefined, needs some debugging

--- src/components/AddMaterialForm/AddMaterialForm.tsx
+++ src/components/AddMaterialForm/AddMaterialForm.tsx
@@ -1,15 +1,67 @@
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import React, { useState } from 'react';
+
+interface FormValues {
+  name: string;
+  quantityLeft: number;
+}
 
 function AddMaterialForm() {
+  const [loading, setLoading] = useState(false)
+  const [formValues, setFormValues] = useState<FormValues>({
+    name: "",
+    quantityLeft: 0
+  });
+  
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    console.log(e.target)
+    setFormValues((prev) => ({ ...prev, [name]: value }));
+    console.log(formValues)
+  };
+
+  
+  const newFormData = () => {
+    const formData = new FormData();
+    formData.append("name", formValues.name);
+    formData.append("quantityLeft", formValues.quantityLeft);
+    return formData;
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      // API call
+      const response = await fetch("http://localhost:3000/material", {
+        method: "POST",
+        credentials: "include",
+        body: newFormData(),
+      });
+      if (response.ok) {
+        console.log('submitted,ok');
+      } else {
+        console.log("Submit failed");
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+  };
+
+
   return (
     <>
+    <form onSubmit={handleSubmit}>
       <FloatingLabel
         controlId="floatingInput"
         label="name"
         className="mb-3"
+        //onChange={handleInputChange}
+        
       >
-        <Form.Control type="text" placeholder="Name" />
+        <Form.Control name="name" type="text" placeholder="Name" onChange={handleInputChange}/>
       </FloatingLabel>
 
       <FloatingLabel
@@ -17,9 +69,16 @@ function AddMaterialForm() {
         label="quantity"
         className="mb-3"
       >
-        <Form.Control type="number" placeholder="Quantity" />
+        <Form.Control name="quantityLeft" type="number" placeholder="Quantity" onChange={handleInputChange}/>
       </FloatingLabel>
       
+      <div className="d-grid gap-2">
+      <Button variant="primary" disabled={loading} type="submit">
+        Submit
+      </Button>
+      
+    </div>
+    </form>
     </>
   );
 }
