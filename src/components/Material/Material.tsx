1bd2fb46dd9c06bf8bf50d7304abfcf241fbcf2b
Valeria Serci <balerty84@gmail.com>
2024-05-29 22:57:54 +0100

    add onDelete, still in progress, error handlers to be added

--- src/components/Material/Material.tsx
+++ src/components/Material/Material.tsx
@@ -1,14 +1,24 @@
 interface MaterialProps {
+  _id: string;
   name: string;
   quantityLeft: number;
 
 }
 
 const Material = (props: MaterialProps) => {
+  const onDelete = () => {
+    fetch(import.meta.env.VITE_BACKEND_URL + `/material/${props._id}`, {
+      method: "DELETE"
+    } )
+      .then(() => {console.log('deleted')})
+    
+  }
 
   return (
     <>
-    <li>{props.name} {props.quantityLeft}</li>
+    <li>{props.name} {props.quantityLeft}
+      <button onClick={onDelete}>Delete</button>
+    </li>
     </>
   )
 }
